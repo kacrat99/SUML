ac49bc01a3ee8ac92810c3f305ec27f724227257
kacrat99 <38194349+kacrat99@users.noreply.github.com>
2021-10-24 14:18:24 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import time
+import matplotlib as plt
+import os
+
+
+
+st.header('translator')
+# header to jeden z podtytułów wykorzystywnaych w Streamlit
+
+st.subheader('O translatorze')
+# subheader to jeden z podtytułów wykorzystywnaych w Streamlit
+
+st.text('jest to przykladowy translator z jezyka angielskiego na niemiecki')
+st.text('aby przetłumaczyć tekst prosze wpisać w polu fraze/zdanie a nastepnie kliknac ctr+enter')
+# text używamy do wyświetlenia dowolnego tekstu. Można korzystać z polskich znaków.
+
+
+import streamlit as st
+from transformers import pipeline,XLMTokenizer, XLMWithLMHeadModel
+
+
+
+option = st.selectbox(
+    "Opcje",
+    [
+        "Angielski -> Niemiecki",
+        "???",
+    ],
+)
+
+if option == "Angielski -> Niemiecki":
+    text = st.text_area(label="Wpisz tekst")
+    if text:
+        translator = pipeline("translation_en_to_de")
+        answer = translator(text)
+        st.code(answer[0]['translation_text'])
+
